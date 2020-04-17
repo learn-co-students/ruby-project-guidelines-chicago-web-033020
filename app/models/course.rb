8ee7f02f6aaeb473081a648880eecd2b684e9fd4
Yann233 <61567922+Yann233@users.noreply.github.com>
2020-04-16 23:34:08 -0500

    update READ

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,20 +2,6 @@ class Course < ActiveRecord::Base
   has_many :records
   has_many :students, through: :records
 
-#   # Show all courses
-#   def self.list_all_courses #READ
-#     puts "\nAvailable courses are: "
-#     course_list.each do |course|
-#     puts "#{course.id}. #{course.name}"
-#     end
-#   end
-# end
-
-# def view_enrolled
-#   puts "\nCourses enrolled is (are):"
-#   self.records.each do |record|
-#   puts "Course ID: #{record.course.id} Course Name: #{record.course.name}" 
-
   def self.list_all_courses #READ
     puts "\nAvailable courses are: "
     course_list = Course.all
